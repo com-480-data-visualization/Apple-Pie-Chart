1ce88214d4790d5917794726f02ca928dd087817
Yiwei <1046792702@qq.com>
2025-04-18 12:43:28 +0200

    Update docs for GitHub Pages

--- website/frontend/app/next.config.mjs
+++ website/frontend/app/next.config.mjs
@@ -3,7 +3,9 @@ const nextConfig = {
   output: 'export',        // use static export
   images: {
     unoptimized: true      // Turn off built-in image optimization
-  }
+  },
+  basePath: '/Apple-Pie-Chart',
+  assetPrefix: '/Apple-Pie-Chart/',
 };
 
 export default nextConfig;
