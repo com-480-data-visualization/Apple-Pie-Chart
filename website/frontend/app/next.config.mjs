6da23b348f4bd4aa70ccd182e93e728c5bc079c6
Yiwei <1046792702@qq.com>
2025-04-18 12:18:17 +0200

    Update website design and layout

--- website/frontend/app/next.config.mjs
+++ website/frontend/app/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export', // Enable static export
-  };
-  
-  export default nextConfig;
+  output: 'export',        // use static export
+  images: {
+    unoptimized: true      // Turn off built-in image optimization
+  }
+};
+
+export default nextConfig;
